fe8c349bcd1902f08e367996eb8e013fd2b36e8f
jaipriyanh <jaipriyanh@gmail.com>
2025-10-19 16:47:50 +0200

    Update alu.vhdl

--- src/alu.vhdl
+++ src/alu.vhdl
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity alu is
-    generic(N: integer := 8);
+    generic(N: positive := 8);
     port (
         clk_i     : in  std_logic;
         res_ni    : in  std_logic;
@@ -26,12 +26,9 @@ begin
     begin
         if res_ni = '0' then
             result_s <= (others => '0');
-            zero_o   <= '0';
             carry_o  <= '0';
-
         elsif rising_edge(clk_i) then
             tmp_carry := '0';
-
             case opcode_i is
                 when "0001" =>  -- Addition (signed)
                     sum_v    := op1_i + op2_i;
@@ -40,7 +37,6 @@ begin
                     if (op1_i(N-1) = op2_i(N-1)) and (sum_v(N-1) /= op1_i(N-1)) then
                         tmp_carry := '1';
                     end if;
-
                 when "0010" =>  -- Subtraction (signed)
                     diff_v   := op1_i - op2_i;
                     result_s <= diff_v;
@@ -48,11 +44,9 @@ begin
                     if (op1_i(N-1) /= op2_i(N-1)) and (diff_v(N-1) /= op1_i(N-1)) then
                         tmp_carry := '1';
                     end if;
-
                 when "0011" =>  -- Multiplication (truncate to N)
                     result_s <= resize(op1_i * op2_i, N);
                     tmp_carry := '0';
-
                 when "0100" =>  -- Division
                     if op2_i /= 0 then
                         result_s <= op1_i / op2_i;
@@ -60,32 +54,27 @@ begin
                         result_s <= (others => '0');
                     end if;
                     tmp_carry := '0';
-
                 when "0101" =>  -- AND
                     result_s <= op1_i and op2_i;
-
+                    tmp_carry := '0';
                 when "0110" =>  -- OR
                     result_s <= op1_i or op2_i;
-
+                    tmp_carry := '0';
                 when "0111" =>  -- XOR
                     result_s <= op1_i xor op2_i;
-
+                    tmp_carry := '0';
                 when others =>
                     result_s <= (others => '0');
                     tmp_carry := '0';
             end case;
-
             carry_o <= tmp_carry;
-
-            if result_s = to_signed(0, N) then
-                zero_o <= '1';
-            else
-                zero_o <= '0';
-            end if;
         end if;
     end process;
-
-    -- drive the output port from the internal register
+    
+    -- Zero flag logic
+    zero_o <= '1' when result_s = to_signed(0, N) else '0';
+    
+    -- Drive output
     result_o <= result_s;
+    
 end behavioral;
-
